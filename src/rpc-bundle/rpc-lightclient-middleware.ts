0d10a2368199ee97cc79962bcd9b856647cb2f1a
Shresth Agrawal <shresthagrawal.31@gmail.com>
2022-10-20 21:39:44 +0200

    feat: add rpc-lightclient-middleware

---
+++ src/rpc-bundle/rpc-lightclient-middleware.ts
@@ -0,0 +1,42 @@
+import { createAsyncMiddleware } from 'json-rpc-engine';
+import { getJSONRPCServer } from '@lightclients/patronum';
+import { ClientManager } from './client-manager.js';
+import { ClientType } from '../constants.js';
+import { defaultBeaconAPIURL, defaultProvers, defaultPublicRPC } from './constants';
+import { EthereumRpcError, ethErrors } from 'eth-rpc-errors';
+
+// TODO: fix types
+function getRPCLightClientMiddleware(network: number) {
+  const clientType = ClientType.optimistic;
+  const beaconAPIURL = defaultBeaconAPIURL[network];
+  const proverURLs = defaultProvers[clientType][network];
+  const providerURL = defaultPublicRPC[network][0]; 
+
+  const cm = new ClientManager(
+    network,
+    clientType,
+    beaconAPIURL,
+    providerURL, 
+    proverURLs,
+  );
+  const syncPromise = cm.sync();
+  let server: any = null;
+
+  return createAsyncMiddleware(async (req: any, res: any, next: any) => {
+    if(server !== null) {
+      const provider = await syncPromise;
+      server = getJSONRPCServer(provider);
+    }
+    const _res = await server.receive(req);
+    // TODO: fix error
+    if(!_res) 
+      throw ethErrors.rpc.internal({
+        message: `something went wrong`,
+      });
+    else if(_res.error)
+      throw ethErrors.rpc.internal({
+        data: _res.error,
+      });
+    res.result = _res;
+  });
+}
