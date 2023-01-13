eadddf35cd120e98fe754bc6a15667b472130a39
pryority <matthewapryor@gmail.com>
2023-01-13 15:01:15 -0500

    refactor light-optimistic/server.ts

--- src/provers/light-optimistic/server.ts
+++ src/provers/light-optimistic/server.ts
@@ -4,7 +4,6 @@ import { MemoryStore } from './memory-store.js';
 import { LightClient } from '../../clients/light/index.js';
 import { getDefaultClientConfig } from '../../rpc-bundle/utils.js';
 import { BeaconAPIProver } from '../beacon-api-light/client.js';
-import { init } from '@chainsafe/bls/switchable';
 
 export function getApp(network: number, beaconAPIURL: string) {
   const app = express();
@@ -14,99 +13,46 @@ export function getApp(network: number, beaconAPIURL: string) {
   const client = new LightClient(config, beaconAPIURL, provers, store);
   client.sync().then(() => client.subscribe(() => {}));
 
-  app.get('/sync-committee/hashes', async (req, res) => {
+  app.get('/sync-committee/hashes', function (req, res, next) {
+    if (!client.isSynced) return res.status(400).json({ error: 'Client not synced' });
+    const startPeriod = parseInt(req.query.startPeriod as string);
+    const maxCount = parseInt(req.query.maxCount as string);
+
     try {
-      if (!client.isSynced) {
-        return res.status(400).json({ error: 'Client not synced' });
-      }
-      const startPeriod = parseInt(req.query.startPeriod as string);
-      const maxCount = parseInt(req.query.maxCount as string);
-      const hashes = store.getCommitteeHashes(startPeriod, maxCount);
-      res.set('Content-Type', 'application/octet-stream');
-      res.end(hashes);
-    } catch (e) {
-      console.error(e);
-      res.status(500).json({ error: e.message });
+        const hashes = store.getCommitteeHashes(startPeriod, maxCount);
+        res.set('Content-Type', 'application/octet-stream');
+        res.end(hashes);
+    } catch (err) {
+        next(err);
     }
   });
 
-  app.get('/sync-committee/:period', async (req, res) => {
+  app.get('/sync-committee/:period', function (req, res, next) {
+    if (!client.isSynced) return res.status(400).json({ error: 'Client not synced' });
+    const period = req.params.period === 'latest' ? client.latestPeriod : parseInt(req.params.period);
+
     try {
-      if (!client.isSynced) {
-        return res.status(400).json({ error: 'Client not synced' });
-      }
-      const period =
-        req.params.period === 'latest'
-          ? client.latestPeriod
-          : parseInt(req.params.period);
-      const committee = store.getCommittee(period);
-      res.set('Content-Type', 'application/octet-stream');
-      res.end(committee);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+        const committee = store.getCommittee(period);
+        res.set('Content-Type', 'application/octet-stream');
+        res.end(committee);
+    } catch (err) {
+        next(err);
     }
   });
 
-  app.get('/sync-update/:period', async (req, res) => {
+  app.get('/sync-update/:period', function (req, res, next) {
+    if (!client.isSynced) return res.status(400).json({ error: 'Client not synced' });
+    const period = req.params.period === 'latest' ? client.latestPeriod : parseInt(req.params.period);
+
     try {
-      if (!client.isSynced) {
-        return res.status(400).json({ error: 'Client not synced' });
-      }
-      const period =
-        req.params.period === 'latest'
-          ? client.latestPeriod
-          : parseInt(req.params.period);
-      const update = store.getUpdate(period);
-      res.set('Content-Type', 'application/octet-stream');
-      res.end(update);
-    } catch (e) {
-      console.error(e);
-      res.status(500).json({ error: e.message });
+        const update = store.getUpdate(period);
+        res.set('Content-Type', 'application/octet-stream');
+        res.end(update);
+    } catch (err) {
+        next(err);
     }
   });
 
   return app;
 }
 
-function handleErrors() {
-  process.on('uncaughtException', (err: Error) => {
-    console.error('uncaughtException', process.pid, err);
-    process.exit(1);
-  });
-
-  process.on('unhandledRejection', (reason: any, promise: any) => {
-    console.error('unhandledRejection', process.pid, { reason, promise });
-    process.exit(1);
-  });
-
-  process.on('SIGTERM', () => {
-    console.log(`Process ${process.pid} exiting (SIGTERM)...`);
-    process.exit();
-  });
-
-  process.on('SIGINT', () => {
-    console.log(`Process ${process.pid} exiting (SIGINT)...`);
-    process.exit();
-  });
-}
-
-export async function startServer(
-  port: number,
-  network: number,
-  beaconAPIURL: string,
-) {
-  handleErrors();
-  try {
-    await init('blst-native');
-  } catch {
-    await init('herumi');
-  }
-  const httpServer = http.createServer();
-
-  httpServer.setTimeout(1000 * 20); // 20s
-  httpServer.on('request', await getApp(network, beaconAPIURL));
-
-  httpServer.listen(port, function () {
-    console.log(`Server listening on port ${port}`);
-  });
-}
