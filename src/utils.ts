7f8d78c1120ac06ba3659ee94d4d5f6f938a8bd6
pryority <matthewapryor@gmail.com>
2023-01-13 19:59:29 -0500

    add: LRU cache

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,46 @@
-import Decimal from 'decimal.js';
 import { toHexString, fromHexString } from '@chainsafe/ssz';
 import bls from '@chainsafe/bls/switchable';
 import _ from 'lodash';
 import axios from 'axios';
+import LRU from 'lru-cache';
+
+const cache = new LRU({ max: 500 });
+// console.log('CACHE',cache)
+
+export async function handleGETRequest(
+  url: string,
+  isBuffer: boolean = true,
+  retry: number = 3,
+): Promise<any> {
+  if (retry < 0) {
+    throw Error(`GET request failed: ${url}`);
+  }
+
+  // Check if the data is already in the cache
+  const cachedData = cache.get(url);
+  if (cachedData) {
+    console.log('cachedData 1', cachedData)
+    return cachedData;
+  }
+
+  try {
+    const { data } = await axios.get(
+      url,
+      isBuffer ? { responseType: 'arraybuffer' } : undefined,
+    );
+    cache.set(url, data);
+    // console.log('cachedData 2',cachedData)
+    console.log('CACHE',cache)
+    console.log('CACHE',cache.keys)
+    return data;
+  } catch (e) {
+    console.error(`failed GET request (${url}): ${e.message}`);
+    return handleGETRequest(url, isBuffer, retry - 1);
+  }
+}
 
 export function logFloor(x: number, base: number = 2) {
-  return Decimal.log(x, base).floor().toNumber();
+  return Math.floor(Math.log(x) / Math.log(base));
 }
 
 export function concatUint8Array(data: Uint8Array[]) {
@@ -19,81 +54,42 @@ export function concatUint8Array(data: Uint8Array[]) {
   return result;
 }
 
-export function isUint8ArrayEq(a: Uint8Array, b: Uint8Array): boolean {
+export const isUint8ArrayEq = (a: Uint8Array, b: Uint8Array): boolean => {
   return toHexString(a) === toHexString(b);
 }
 
-export function isCommitteeSame(a: Uint8Array[], b: Uint8Array[]): boolean {
+export const isCommitteeSame = (a: Uint8Array[], b: Uint8Array[]): boolean => {
   if (a.length !== b.length) return false;
   return a.every((c, i) => isUint8ArrayEq(c, b[i]));
 }
 
-export function generateRandomSyncCommittee(): Uint8Array[] {
-  let res = [];
-  // TODO: change 512 to constant
-  for (let i = 0; i < 512; i++) {
-    res.push(bls.SecretKey.fromKeygen().toPublicKey().toBytes());
-  }
-  return res;
+export const generateRandomSyncCommittee = (): Uint8Array[] => {
+  return Array.from({length: 512}, () => bls.SecretKey.fromKeygen().toPublicKey().toBytes());
 }
 
-export function getRandomInt(max: number) {
+export const getRandomInt = (max: number) => {
   return Math.floor(Math.random() * max);
 }
 
 export const smallHexStr = (data: Uint8Array) => toHexString(data).slice(0, 8);
 
-export function numberToUint8Array(num: number): Uint8Array {
-  const rawHex = num.toString(16);
-  const hex = '0x' + (rawHex.length % 2 === 0 ? rawHex : '0' + rawHex);
-  return fromHexString(hex);
+export const numberToUint8Array = (num: number): Uint8Array => {
+  return new Uint8Array(
+    Array.from({length: 8}, (_, i) => (num >> (8 * i)) & 0xff)
+    .reverse()
+  );
 }
 
-// credit: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-export function shuffle(array: any[]) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
+export const shuffle = (array: any[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-
   return array;
 }
 
-export async function wait(ms: number) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-export async function handleGETRequest(
-  url: string,
-  isBuffer: boolean = true,
-  retry: number = 3,
-): Promise<any> {
-  if (retry < 0) {
-    throw Error(`GET request failed: ${url}`);
-  }
-  try {
-    const { data } = await axios.get(
-      url,
-      isBuffer ? { responseType: 'arraybuffer' } : undefined,
-    );
-    return data;
-  } catch (e) {
-    console.error(`failed GET request (${url}): ${e.message}`);
-    return handleGETRequest(url, isBuffer, retry - 1);
-  }
+export const wait = async (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export function deepTypecast<T>(
@@ -101,13 +97,9 @@ export function deepTypecast<T>(
   checker: (val: any) => boolean,
   caster: (val: T) => any,
 ): any {
-  return _.forEach(obj, (val: any, key: any, obj: any) => {
-    obj[key] = checker(val)
-      ? caster(val)
-      : _.isObject(val)
-      ? deepTypecast(val, checker, caster)
-      : val;
-  });
+  return _.mapValues(obj, (val) => checker(val) 
+  ? caster(val) 
+  : deepTypecast(val, checker, caster));
 }
 
 // export class RandomBytesGenerator {
