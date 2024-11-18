af6c950f64b6899280e905735bcee095828340b1
rista404 <ristic@risticnikola.com>
2024-11-18 14:32:21 +0100

    add deploy sepolia heroku script

--- src/provers/light-optimistic/deploy-heroku-sepolia.sh
+++ src/provers/light-optimistic/deploy-heroku-sepolia.sh
@@ -3,6 +3,6 @@ name=$1
 heroku create "$name"
 git remote add "$name" "https://git.heroku.com/$name.git"
 
-heroku config:set --app "$name" BEACON_CHAIN_API_URL=https://lodestar-mainnet.chainsafe.io CHAIN_ID=1
+heroku config:set --app "$name" BEACON_CHAIN_API_URL=https://lodestar-sepolia.chainsafe.io CHAIN_ID=11155111
 
 git push "$name" main
